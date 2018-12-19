ef777c6326b6640087567cf57cbd84823281765a
avichayt <avichay.radovsky@mail.huji.ac.il>
2018-12-19 22:23:06 +0200

    Vector class done(?)

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -4,6 +4,7 @@
 
 #include "Vector3D.h"
 #include <cmath>
+#include <iostream>
 
 Vector3D::Vector3D(double x, double y, double z)
 {
@@ -55,7 +56,7 @@ Vector3D &Vector3D::operator-=(const Vector3D &other)
     return *this;
 }
 
-Vector3D &Vector3D::operator+=(const int other)
+Vector3D &Vector3D::operator+=(const double other)
 {
     _x += other;
     _y += other;
@@ -64,7 +65,7 @@ Vector3D &Vector3D::operator+=(const int other)
     return *this;
 }
 
-Vector3D &Vector3D::operator-=(const int other)
+Vector3D &Vector3D::operator-=(const double other)
 {
     _x -= other;
     _y -= other;
@@ -73,7 +74,7 @@ Vector3D &Vector3D::operator-=(const int other)
     return *this;
 }
 
-Vector3D &Vector3D::operator*=(const int other)
+Vector3D &Vector3D::operator*=(const double other)
 {
     _x *= other;
     _y *= other;
@@ -82,7 +83,7 @@ Vector3D &Vector3D::operator*=(const int other)
     return *this;
 }
 
-Vector3D &Vector3D::operator/=(const int other)
+Vector3D &Vector3D::operator/=(const double other)
 {
     _x /= other;
     _y /= other;
@@ -97,19 +98,19 @@ Vector3D Vector3D::operator-()
     return result;
 }
 
-Vector3D Vector3D::operator*(int number)
+Vector3D Vector3D::operator*(double number)
 {
     Vector3D result(_x * number, _y * number, _z * number);
     return result;
 }
 
-Vector3D Vector3D::operator/(int number)
+Vector3D Vector3D::operator/(double number)
 {
     Vector3D result(_x / number, _y / number, _z / number);
     return result;
 }
 
-Vector3D operator*(int left, const Vector3D &right)
+Vector3D operator*(double left, const Vector3D &right)
 {
     Vector3D result(right._x * left, right._y * left, right._z * left);
     return result;
@@ -117,9 +118,71 @@ Vector3D operator*(int left, const Vector3D &right)
 
 double Vector3D::dist(Vector3D &other)
 {
-    Vector3D sub(*this - other);
-    return sqrt(())
+    const Vector3D &sub = *this - other;
+    return sqrt(sub._x * sub._x + sub._y * sub._y + sub._z * sub._z);
 }
+
+double Vector3D::norm()
+{
+    Vector3D origin(0, 0, 0);
+    return dist(origin);
+}
+
+double Vector3D::operator|(Vector3D &other)
+{
+    return dist(other);
+}
+
+double Vector3D::operator*(Vector3D &other)
+{
+    return _x * other._x + _y * other._y + _z * other._z;
+}
+
+double Vector3D::operator^(Vector3D &other)
+{
+    return ((*this) * other) / norm() / other.norm();
+}
+
+std::istream &operator>>(std::istream &stream, Vector3D &vector)
+{
+    return stream >> vector._x >> vector._y >> vector._z;
+}
+
+std::ostream &operator<<(std::ostream &stream, const Vector3D &vector)
+{
+    return stream << vector._x << vector._y << vector._z << std::endl;
+}
+
+Vector3D &Vector3D::operator=(const Vector3D &other)
+{
+    _x = other._x;
+    _y = other._y;
+    _z = other._z;
+}
+
+double Vector3D::operator[](int i) const
+{
+    return (*this)[i];
+}
+
+double &Vector3D::operator[](int i)
+{
+    switch (i){
+        case 0:
+            return _x;
+        case 1:
+            return _y;
+        case 2:
+            return _z;
+        default:
+            std::cerr << INDEX_OUT_OF_BOUNDS_ERROR;
+            exit(EXIT_FAILURE);
+    }
+}
+
+
+
+
 
 
 
--- Vector3D.h
+++ Vector3D.h
@@ -5,7 +5,11 @@
 #ifndef EX1_VECTOR3D_H
 #define EX1_VECTOR3D_H
 
+#include <iosfwd>
+
 #define VECTOR_SIZE 3
+
+#define INDEX_OUT_OF_BOUNDS_ERROR "Error: index out of bounds."
 
 class Vector3D
 {
@@ -18,7 +22,7 @@ public:
 
     Vector3D(Vector3D &copyVector);
 
-    double dist(Vector3D& other);
+    double dist(Vector3D &other);
 
     double norm();
 
@@ -30,22 +34,37 @@ public:
 
     Vector3D &operator-=(const Vector3D &other);
 
-    Vector3D &operator*=(int other);
+    Vector3D &operator*=(double other);
 
-    Vector3D &operator/=(int other);
+    Vector3D &operator/=(double other);
 
-    Vector3D &operator+=(int other);
+    Vector3D &operator+=(double other);
 
-    Vector3D &operator-=(int other);
+    Vector3D &operator-=(double other);
 
     Vector3D operator-();
 
-    Vector3D operator*(int number);
+    Vector3D operator*(double number);
 
-    Vector3D operator/(int number);
+    Vector3D operator/(double number);
 
-    friend Vector3D operator*(int left, const Vector3D& right);
+    double operator|(Vector3D &other);
 
+    double operator*(Vector3D &other);
+
+    double operator^(Vector3D &other);
+
+    friend Vector3D operator*(double left, const Vector3D &right);
+
+    friend std::istream &operator>>(std::istream &stream, Vector3D &vector);
+
+    friend std::ostream &operator<<(std::ostream &stream, const Vector3D &vector);
+
+    Vector3D &operator=(const Vector3D &other);
+
+    double operator[](int i) const;
+
+    double & operator[] (int i);
 
 
 private:
