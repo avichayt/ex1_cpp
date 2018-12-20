dad8fb4765126cb07d1f752d073aec218c5967ea
avichayt <avichay.radovsky@mail.huji.ac.il>
2018-12-20 16:11:04 +0200

    more comments.

--- Vector3D.h
+++ Vector3D.h
@@ -1,6 +1,4 @@
-//
-// Created by USER on 12/16/2018.
-//
+// Vector3D.h
 
 //todo warnings check
 // todo comments
@@ -8,6 +6,7 @@
 
 #ifndef EX1_VECTOR3D_H
 #define EX1_VECTOR3D_H
+
 
 #include <iosfwd>
 
@@ -17,45 +16,139 @@
 
 #define DIVISION_BY_ZERO_ERROR "Error: Division by zero."
 
+/**
+ *  A 3D vector class.
+ *  This class represents a vector with (x,y,z) coordinates.
+ */
 class Vector3D
 {
 public:
+    /**
+     * constructor for the vector.
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     */
     Vector3D(double x, double y, double z);
 
+    /**
+     * Constructor that takes an array.
+     * @param array array to construct from
+     */
     Vector3D(double array[VECTOR_SIZE]);
 
+    /**
+     * default constructor for the vector.
+     */
     Vector3D();
 
+    /**
+     * copy constructor
+     * @param copyVector vector to copy
+     */
     Vector3D(const Vector3D &copyVector);
 
+    /**
+     * get distance from other vector
+     * @param other the other vector
+     * @return the distance
+     */
     double dist(const Vector3D &other) const;
 
+    /**
+     * get the norm of the vector
+     * @return the norm
+     */
     double norm() const;
 
+    /**
+     * overload for plus operator
+     * @param other the other vector
+     * @return the result
+     */
     Vector3D operator+(const Vector3D &other) const;
 
+    /**
+     * overload for minus operator
+     * @param other the other vector
+     * @return the result
+     */
     Vector3D operator-(const Vector3D &other) const;
 
+    /**
+     * overload for plus equals operator
+     * @param other the other vector
+     * @return this vector
+     */
     Vector3D &operator+=(const Vector3D &other);
 
+    /**
+     * overload for minus equals operator
+     * @param other the other vector
+     * @return this vector
+     */
     Vector3D &operator-=(const Vector3D &other);
 
+    /**
+     * overload for * equals operator
+     * @param other a number
+     * @return this vector after calc
+     */
     Vector3D &operator*=(double other);
 
+    /**
+     * overload for / equals operator
+     * @param other a number
+     * @return this vector after calc
+     */
     Vector3D &operator/=(double other);
 
+    /**
+     * overload for + equals operator
+     * @param other a number
+     * @return this vector after calc
+     */
     Vector3D &operator+=(double other);
 
+    /**
+     * overload for - equals operator
+     * @param other a number
+     * @return this vector after calc
+     */
     Vector3D &operator-=(double other);
 
+    /**
+     * unary minus operator overload
+     * @return result
+     */
     Vector3D operator-();
 
+    /**
+     * multiply vecotr by number
+     * @param number num to mult
+     * @return result
+     */
     Vector3D operator*(double number);
 
+    /**
+     * divide vector by num
+     * @param number num to divide
+     * @return result
+     */
     Vector3D operator/(double number);
 
+    /**
+     * get distance to other vector
+     * @param other the other vector
+     * @return distance
+     */
     double operator|(const Vector3D &other);
 
+    /**
+     * dot product of two vectors
+     * @param other the other vectors
+     * @return result
+     */
     double operator*(const Vector3D &other);
 
     double operator^(const Vector3D &other);
@@ -70,7 +163,7 @@ public:
 
     double operator[](int i) const;
 
-    double & operator[] (int i);
+    double &operator[](int i);
 
 private:
     double _x;
